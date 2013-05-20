bb1aa465add870181b8a146b66ef8a3f6eab40b7
ktoonsez <ktoonsez@gmail.com>
2013-05-20 09:59:14 -0700

    Updates to TW ramdisks

--- TW_SPR/init.qcom.syspart_fixup.sh
+++ TW_SPR/init.qcom.syspart_fixup.sh
@@ -69,9 +69,9 @@ if [ -f /system/etc/init.qcom.mdm_links.sh ]; then
 fi
 
 # Run thermal script
-if [ -f /system/etc/init.qcom.thermal_conf.sh ]; then
-  /system/bin/sh /system/etc/init.qcom.thermal_conf.sh
-fi
+#if [ -f /system/etc/init.qcom.thermal_conf.sh ]; then
+#  /system/bin/sh /system/etc/init.qcom.thermal_conf.sh
+#fi
 
 # Run wifi script
 if [ -f /system/etc/init.qcom.wifi.sh ]; then

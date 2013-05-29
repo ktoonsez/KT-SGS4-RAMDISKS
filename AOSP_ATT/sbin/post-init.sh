18d6196064ea34ec18eda02aafad773162d70ddb
Matt Filetto <matt.filetto@gmail.com>
2013-05-28 19:57:43 -0700

    post-init.sh: enable OC options by default

--- AOSP_ATT/sbin/post-init.sh
+++ AOSP_ATT/sbin/post-init.sh
@@ -3,5 +3,6 @@
 #Set governor items
 echo 1890000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq;
 echo 1 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_booted;
+echo 1 > /sys/devices/system/cpu/cpufreq/ktoonsez/enable_oc;
 
 echo $(date) END of post-init.sh

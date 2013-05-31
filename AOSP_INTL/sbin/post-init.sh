c12869d50694768f02232f5d93eabf5241a28385
Matt Filetto <matt.filetto@gmail.com>
2013-05-31 02:35:35 -0700

    AOSP_INTL: bring in sync with ATT

--- AOSP_INTL/sbin/post-init.sh
+++ AOSP_INTL/sbin/post-init.sh
@@ -1,7 +1,13 @@
 #!/system/bin/sh
 
+#!/system/bin/sh
+
+echo 450000000 > /sys/devices/platform/kgsl-3d0.0/kgsl/kgsl-3d0/max_gpuclk
+
 #Set governor items
+echo 378000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq
 echo 1890000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq;
 echo 1 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_booted;
+echo 1 > /sys/devices/system/cpu/cpufreq/ktoonsez/enable_oc;
 
 echo $(date) END of post-init.sh

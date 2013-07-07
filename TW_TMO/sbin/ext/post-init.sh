64f677e049f35f15e0a457d594165f29346c196e
ktoonsez <ktoonsez@gmail.com>
2013-07-07 10:54:05 -0700

    exFAT: Add support to AOSP using Talustus files and mod post-init to mount properly for AOSP.  Also modded post-init for TW to mount both ways a sd card can be formattted

--- TW_TMO/sbin/ext/post-init.sh
+++ TW_TMO/sbin/ext/post-init.sh
@@ -32,15 +32,24 @@ echo "Running Post-Init Script"
 
 ## Testing: Check for ExFat SD Card
 #
-SDTYPE=`blkid /dev/block/mmcblk1p1  | awk '{ print $3 }' | sed -e 's|TYPE=||g' -e 's|\"||g'`
+SDTYPE1=`blkid /dev/block/mmcblk1  | awk '{ print $3 }' | sed -e 's|TYPE=||g' -e 's|\"||g'`
+SDTYPE2=`blkid /dev/block/mmcblk1p1  | awk '{ print $3 }' | sed -e 's|TYPE=||g' -e 's|\"||g'`
 
-if [ ${SDTYPE} == "exfat" ];
+if [ "${SDTYPE1}" == "exfat" ];
+then
+  echo "ExFat-Debug: SD-Card is type ExFAT"
+  echo "ExFat-Debug: trying to mount via fuse"
+  mount.exfat-fuse /dev/block/mmcblk1 /storage/extSdCard
+else
+  echo "ExFat-Debug: SD-Card is type1: ${SDTYPE1}"
+fi
+if [ "${SDTYPE2}" == "exfat" ];
 then
   echo "ExFat-Debug: SD-Card is type ExFAT"
   echo "ExFat-Debug: trying to mount via fuse"
   mount.exfat-fuse /dev/block/mmcblk1p1 /storage/extSdCard
 else
-  echo "ExFat-Debug: SD-Card is type: ${SDTYPE}"
+  echo "ExFat-Debug: SD-Card is type2: ${SDTYPE2}"
 fi
 
 # Remount FileSys RO

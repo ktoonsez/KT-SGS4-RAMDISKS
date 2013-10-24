b7655f4e0ed4e1e01eb3276c9d1020da7be1c77b
ktoonsez <ktoonsez@gmail.com>
2013-10-23 19:24:42 -0700

    Remove old jumk and add MJ5 TW4.3 ramdisks

--- TW_ATT4.3/init.qcom.syspart_fixup.sh
+++ TW_ATT4.3/init.qcom.syspart_fixup.sh
@@ -33,14 +33,29 @@ serial="$2"
 PATH=/sbin:/system/sbin:/system/bin:/system/xbin
 export PATH
 
-mount_needed=false;
+# sys_sw.sa: Check if symbolic links were already created
+if [ -f /firmware/image/tima_pkm.mdt ]; then
+	if [ -f /system/etc/firmware/sbl1.mbn ] && [ -f /system/etc/firmware/q6.mdt ] && [ -f /system/etc/firmware/tima_pkm.mdt ]; then
+	  echo "init: /init.qcom.syspart_fixup.sh: tima2.0 skipped because symbolic links alreay exist" > /dev/kmsg
+	  exit 0
+	fi
+else
+	if [ -f /firmware/image/tima.mdt ]; then
+		if [ -f /system/etc/firmware/sbl1.mbn ] && [ -f /system/etc/firmware/q6.mdt ] && [ -f /system/etc/firmware/tima.mdt ]; then
+		  echo "init: /init.qcom.syspart_fixup.sh: tima skipped because symbolic links alreay exist" > /dev/kmsg
+		  exit 0
+		fi
+	else
+		if [ -f /system/etc/firmware/sbl1.mbn ] && [ -f /system/etc/firmware/q6.mdt ]; then
+		  echo "init: /init.qcom.syspart_fixup.sh: non-tima skipped because symbolic links alreay exist" > /dev/kmsg
+		  exit 0
+		fi
+	fi
+fi
 
-if [ ! -f /system/etc/boot_fixup ];then
 # This should be the first command
 # remount system as read-write.
-  mount -o rw,remount,barrier=1 /system
-  mount_needed=true;
-fi
+mount -o rw,remount,barrier=1 /system
 
 # **** WARNING *****
 # This runs in a single-threaded, critical path portion
@@ -77,7 +92,5 @@ fi
 
 # This should be the last command
 # remount system as read-only.
-  mount -o ro,remount,barrier=1 /system
-
-
+mount -o ro,remount,barrier=1 /system
 
